9573a5e5ba9141a897f8539292b480c565ebd819
BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>
2022-05-05 17:25:53 +0200

    dhcp_client_utils: send option 60 and 43 in DHCPv4 discover message ( OFW-2566 F5685SI-19 ) - part 3
    DHCPv4 option 60 and 43 support for EthWan platform
    
    [Issue Description]
    
    Support for sending vendor specific information for RG services - XGSPON is to be implemented.
    
    [Fix description]
    
    Added Support to send Option 60 and Option 43 vendor specific details.
    
    [Testing Performed]
    
    Verfied option 60 and 43 from DHCPv4 packets of wan interface.
    
    Signed-off-by: BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>

--- source/dhcp_client_utils/udhcpc_client_utils.c
+++ source/dhcp_client_utils/udhcpc_client_utils.c
@@ -126,25 +126,27 @@ static int udhcpc_get_send_options (char * buff, dhcp_opt_list * send_opt_list)
         return SUCCESS;
     }
 
-    char args [BUFLEN_512] = {0};
     while ((send_opt_list != NULL) && (send_opt_list->dhcp_opt_val != NULL))
     {
-        memset (&args, 0, BUFLEN_512);
+        char args[512];
+
+        args[0] = 0;
+
         if (send_opt_list->dhcp_opt == DHCPV4_OPT_60)
         {
             // Option 60 - Vendor Class Identifier has udhcp cmd line arg "-V <option-str>"
-            snprintf (args, BUFLEN_128, "-V \"%s\" ", send_opt_list->dhcp_opt_val);
+            snprintf (args, sizeof(args), "-V \"%s\" ", send_opt_list->dhcp_opt_val);
         }
-        else if ( send_opt_list->dhcp_opt == DHCPV4_OPT_43)
+        else if (send_opt_list->dhcp_opt == DHCPV4_OPT_43)
         {
-            snprintf(args, BUFLEN_512, "-x %d:%s ", send_opt_list->dhcp_opt, send_opt_list->dhcp_opt_val);
+            snprintf(args, sizeof(args), "-x %d:%s ", send_opt_list->dhcp_opt, send_opt_list->dhcp_opt_val);
         }
         else
         {
             char * buffer = ascii_to_hex (send_opt_list->dhcp_opt_val, strlen(send_opt_list->dhcp_opt_val));
             if (buffer != NULL)
             {
-                snprintf (args, BUFLEN_128, "-x 0x%02X:%s ", send_opt_list->dhcp_opt, buffer);
+                snprintf (args, sizeof(args), "-x 0x%02X:%s ", send_opt_list->dhcp_opt, buffer);
                 free(buffer);
             }
         }
@@ -230,7 +232,7 @@ pid_t start_udhcpc (dhcp_params * params, dhcp_opt_list * req_opt_list, dhcp_opt
         return FAILURE;
     }
 
-    char buff [BUFLEN_1024] = {0};
+    char buff[1024] = {0};
 
     DBG_PRINT("%s %d: Constructing REQUEST option args to udhcpc.\n", __FUNCTION__, __LINE__);
     if ((req_opt_list != NULL) && (udhcpc_get_req_options(buff, req_opt_list)) != SUCCESS)
