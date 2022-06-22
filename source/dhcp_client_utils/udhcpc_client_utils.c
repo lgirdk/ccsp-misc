865c14fad83a87554cfeac6d9db446627e8d940e
Cibil <cpankiras.contractor@libertyglobal.com>
2022-06-22 17:18:37 +0200

    dhcp_client_utils: find udhcpc pid by name only ( ie not by interface )
    Upstream Candidate: No. LGI specific fix. udhpc only runs on erouter0 in Mv3
    
    Signed-off-by: Cibil <cpankiras.contractor@libertyglobal.com>

--- source/dhcp_client_utils/udhcpc_client_utils.c
+++ source/dhcp_client_utils/udhcpc_client_utils.c
@@ -283,10 +283,8 @@ int stop_udhcpc (dhcp_params * params)
     }
 
     pid_t pid = 0;
-    char cmdarg[BUFLEN_32] = {0};
 
-    snprintf(cmdarg, sizeof(cmdarg), "%s", params->ifname);
-    pid = get_process_pid(UDHCPC_CLIENT, cmdarg);
+    pid = get_process_pid(UDHCPC_CLIENT, NULL);
 
     if (pid <= 0)
     {
