cfcf6c24f746252218d10f483a872b30ab80198a
Karthik Shanbhag <kshanbhag@libertyglobal.com>
2022-11-15 17:25:30 +0100

    dhcp_client_utils: modify dhcpv4 client suboptions to send vendor information ( OFW-3265 )
    use option 125 instead of option 43 by dhcpv4 client to send manufacturer/product and model informations
    as per TR-124.
    
    Change the type of option 61 from 00 to 255 as per RFC4361 instead of
    RFC3315.
    
    Sub-options of Vendor-Identifying Vendor-Specific Information Option 125 in not send in DHCPv4.
    
    [Issue]
    Option 125 was not implemented
    
    [Description]
    This OFW was in CCB. As per the instruction following changes are made. MVXREQ-3598
    1. Remove send option 43. As its cable lab specific.
    2. Add option 125
    3. Remove 125 from the request option 55.
    4. Change the type of option 61 from 00 to 255.
    
    [Solution]
    Implement changes as per MVXREQ-3598
    
    [Testing]
    1. Boot up the gateway
    2. Check the bootp options are right as per the description
    
    Upstream Candidate: No. LGI specific DHCP requirement.
    
    Signed-off-by: Karthik Shanbhag(kshanbhag@libertyglobal.com)

--- source/dhcp_client_utils/dhcpv4_client_utils.c
+++ source/dhcp_client_utils/dhcpv4_client_utils.c
@@ -44,33 +44,6 @@ static pid_t start_ti_udhcpc (dhcp_params * params)
 }
 #endif  // DHCPV4_CLIENT_TI_UDHCPC
 
-/***
- * Parses a file containing vendor specific options
- *
- * options:  buffer containing the returned parsed options
- * length:   length of options
- *
- * Option code 43 - DHCPv4 Vendor- Vendor Specific Information Options
- *            Sub-option code 2,  <Device type> - EROUTER
- *            Sub-option code 3,  ECM:<eSAFE1:eSAFE2...eSAFEn>
- *            Sub-option code 4,  <device serial number>
- *            Sub-option code 5,  <Hardware version>
- *            Sub-option code 6,  <Software version>
- *            Sub-option code 7,  <Boot ROM version>  <-- Boot ROM version is mostly meaningless, assume this actually means Bootloader version.
- *            Sub-option code 8,  <OUI>
- *            Sub-option code 9,  <Model Number>
- *            Sub-option code 10, <Vendor name>
- *            Sub-option code 15, eSAFEs with cfg file encapsulation: <eSAFE1:eSAFE2...eSAFEn>
- *
- *      Code    Type   Len   Val   Type Len      Val    Type
- *     +-----+-------+-----+-----+----+----+-----------+----+--------
- *     | 43  |   2   |  3  | ECM |  3 |  8 |  ECM:EMTA |  4 | . . . .
- *     +-----+-------+-----+-----+----+----+-----------+----+---------
- *
- *
- * returns:  0 on successful parsing, else -1
- ***/
-
 static int verifyBufferSpace(const int length, int opt_len, int size)
 {
     if (length - opt_len <= size) {
@@ -95,129 +68,116 @@ static int writeTOHexFromAscii(char *options, const int length, int opt_len, cha
     return opt_len;
 }
 
-static int prepare_dhcp61_optvalue(char *options, const int length)
+/***
+// RFC 3925 - Vendor-Identifying Vendor Options
+// The format of the V-I Vendor-specific Information option is a follows:
+//                        1 1 1 1 1 1
+//    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5
+//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//   |  option-code  |  option-len   |
+//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//   |      enterprise-number1       |
+//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//   |   data-len1   |               |
+//   +-+-+-+-+-+-+-+-+ option-data1  |
+//   /                               /
+//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ ----
+//   |      enterprise-number2       |   ^
+//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+   |
+//   |   data-len2   |               | optional
+//   +-+-+-+-+-+-+-+-+ option-data2  |   |
+//   /                               /   |
+//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+   |
+//   ~            ...                ~   V
+//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ ----
+//
+//   option-code         OPTION_V-I_VENDOR_OPTS (125)
+//   option-len          total length of all following option data in octets
+//   enterprise-numberN  The vendor's registered 32-bit Enterprise Number as registered with IANA [3]
+//   data-lenN           Length of option-data field
+//   option-dataN        Vendor-specific options, described below
+//   data-len1           Size of the option-data1
+//
+//   option-data1 format as follows
+//                        1 1 1 1 1 1
+//    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5
+//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//   |  subopt-code  |  subopt-len   |
+//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//   /        sub-option-data        /
+//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+***/
+
+static int prepare_dhcp125_optvalue(char *options_125, const int length)
 {
-    char basemac[18], modelName[32], SerialNo[64], IAID[9], buf[256];
     int opt_len = 0;
-
-    opt_len = sprintf(options, "00"); //add type, for now 00
-
-    if(platform_hal_GetBaseMacAddress(basemac) == RETURN_OK)
-    {
-        IAID[0] = basemac[6];
-        IAID[1] = basemac[7];
-        IAID[2] = basemac[9];
-        IAID[3] = basemac[10];
-        IAID[4] = basemac[12];
-        IAID[5] = basemac[13];
-        IAID[6] = basemac[15];
-        IAID[7] = basemac[16];
-    }
-
-    //Identifier in format <OUI>"-"<ProductClass>"-"<CPE_LogisticsSerialNumber>
-
-    if(platform_hal_GetProductClass(modelName) != RETURN_OK)  //Product class is MERCV3X
-    {
-        DBG_PRINT("Failed to get ModelName \n");
-        return -1;
-    }
-
-    if(platform_hal_GetSerialNumber(SerialNo) != RETURN_OK) //Serial Number
-    {
-        DBG_PRINT("Failed to get SerialNumber \n");
-        return -1;
-    }
-
-    snprintf(buf, sizeof(buf),"%s%s%s-%s-%s",IAID,DUID,CONFIG_VENDOR_ID,modelName,SerialNo);
-    opt_len = writeTOHexFromAscii(options, length, opt_len, buf);
-
-    return 0;
-}
-/*
-   Warning: This function should be kept aligned with dhcp_parse_vendor_info() in utopia/source/service_wan/service_wan.c
-*/
-static int prepare_dhcp43_optvalue( char *options, const int length, char *ethWanMode )
-{
-    FILE *fp;
-    char subopt_num[12] ={0}, subopt_value[64] = {0} , mode[8] = {0} ;
-    int num_read;
-    char buf[64];
-    int opt_len = 0;   //Total characters read
+    int padding = 0;
+    char duid [64];
+    char options[128];
     int subopt;
+    char buf[64];
     size_t len;
 
-    *options = 0;
+    //Create enterprise number
+    snprintf(duid, sizeof(duid),"%02x%02x%x%02x",padding,padding,padding,atoi(DUID));
 
-    if ((fp = fopen(VENDOR_SPEC_FILE, "ra")) != NULL) {
-        while ((num_read = fscanf(fp, "%7s %11s %63s", mode, subopt_num, subopt_value)) == 3) {
-            if (length - opt_len < 6) {
-                DBG_PRINT("%s: Too many options\n", __FUNCTION__ );
-                fclose(fp);   //CID 61631 : Resource leak
-                return -1;
-            }
-
-            if ( ( strcmp(mode,"DOCSIS") == 0 ) && ( strcmp (ethWanMode,"true") == 0) )
-            {
-                continue;
-            }
-
-            if ( ( strcmp(mode,"ETHWAN") == 0 ) && ( strcmp (ethWanMode,"false") == 0) )
-            {
-                continue;
-            }
-
-            //Print the option number
-            if (strcmp(subopt_num, "SUBOPTION1") == 0) {
-                if (!verifyBufferSpace(length, opt_len, 2)) {
-                    fclose(fp);
-                    return -1;
-                }
-                opt_len += sprintf(options + opt_len, "01");
-            }
-            else if (strcmp(subopt_num, "SUBOPTION2") == 0) {
-                if (!verifyBufferSpace(length, opt_len, 2)) {
-                    fclose(fp);
-                    return -1;
-                }
-                opt_len += sprintf(options + opt_len, "02");
-            }
-            else if (strcmp(subopt_num, "SUBOPTION3") == 0) {
-                if (!verifyBufferSpace(length, opt_len, 2)) {
-                    fclose(fp);
-                    return -1;
-                }
-                opt_len += sprintf(options + opt_len, "03");
-            }
-            else {
-                DBG_PRINT( "%s: Invalid suboption\n", __FUNCTION__ );
-                fclose(fp);
-                return -1;
-            }
-
-            //Print the length of the sub-option value
-            if (!verifyBufferSpace(length, opt_len, 2)) {
-                fclose(fp);
-                return -1;
-            }
-            opt_len += sprintf(options + opt_len, "%02x", strlen(subopt_value));
-
-            //Print the sub-option value in hex
-            opt_len = writeTOHexFromAscii(options, length, opt_len, subopt_value);
-            if (opt_len == 0) {
-                fclose(fp);
-                return -1;
-            }
-        } //while
-
-        fclose(fp);
-
-        if ((num_read != EOF) && (num_read != 3)) {
-            DBG_PRINT("%s: Error parsing file\n", __FUNCTION__);
+    //Create option-data1
+    //Suboptions as per MVXREQ-3598 
+    /*
+       Sub-option code 1 - Manufacturer OUI 
+    */
+    subopt = 1;
+    len = strlen (CONFIG_VENDOR_ID);
+    if (len > 0)
+    {
+        if ((len > 0xFF) || !verifyBufferSpace(length, opt_len, 2 + 2 + (2 * len))) {
             return -1;
         }
+        opt_len += sprintf(options + opt_len, "%02x%02x", subopt, len);
+        opt_len = writeTOHexFromAscii(options, length, opt_len, CONFIG_VENDOR_ID);
     }
-    else {
-        DBG_PRINT("%s: Cannot read %s\n", __FUNCTION__, VENDOR_SPEC_FILE);
+    /*
+       Sub-option code 2 -  Product Class 
+    */
+
+    if(platform_hal_GetProductClass(buf) == RETURN_OK)
+    {
+        subopt = 2;
+        len = strlen (buf);
+        if (len > 0)
+        {
+            if ((len > 0xFF) || !verifyBufferSpace(length, opt_len, 2 + 2 + (2 * len))) {
+              return -1;
+        }
+            opt_len += sprintf(options + opt_len, "%02x%02x", subopt, len);
+            opt_len = writeTOHexFromAscii(options, length, opt_len, buf);
+        }
+    }
+    else
+    {
+        DBG_PRINT("Option 125, Failed to get ProductClass \n");
+        return -1;
+    }
+
+    /*
+       Sub-option code 3 - Model Number
+    */
+    if (platform_hal_GetModelName(buf) == RETURN_OK)
+    {
+        subopt = 3;
+        len = strlen (buf);
+        if (len > 0)
+        {
+            if ((len > 0xFF) || !verifyBufferSpace(length, opt_len, 2 + 2 + (2 * len))) {
+              return -1;
+        }
+            opt_len += sprintf(options + opt_len, "%02x%02x", subopt, len);
+            opt_len = writeTOHexFromAscii(options, length, opt_len, buf);
+        }
+    }
+    else
+    {
+        DBG_PRINT("Option 125, Failed to get ModelName \n");
         return -1;
     }
 
@@ -237,116 +197,56 @@ static int prepare_dhcp43_optvalue( char *options, const int length, char *ethWa
             opt_len = writeTOHexFromAscii(options, length, opt_len, buf);
         }
     }
-
-    /*
-       Sub-option code 5 - Hardware version
-    */
-    if (platform_hal_GetHardwareVersion (buf) == RETURN_OK)
+    else
     {
-        subopt = 5;
-        len = strlen (buf);
-        if (len > 0)
-        {
-            if ((len > 0xFF) || !verifyBufferSpace(length, opt_len, 2 + 2 + (2 * len))) {
-                return -1;
-            }
-            opt_len += sprintf(options + opt_len, "%02x%02x", subopt, len);
-            opt_len = writeTOHexFromAscii(options, length, opt_len, buf);
-        }
+        DBG_PRINT("Option 125, Failed to get SerialNumber \n");
+        return -1;
+    }
+    len = strlen(options);
+    snprintf(options_125,length,"%s%02x%s",duid,(len/2),options);
+    return 0;
+}
+
+static int prepare_dhcp61_optvalue(char *options, const int length)
+{
+    char basemac[18], ProductClass[32], SerialNo[64], IAID[9], buf[256];
+    int type = 255;
+    ULONG IAID_HEX = 0;
+    int padding = 0;
+    int opt_len = 0;
+    int DUID_TYPE = 2;
+
+    if(platform_hal_GetBaseMacAddress(basemac) == RETURN_OK)
+    {
+        IAID[0] = basemac[6];
+        IAID[1] = basemac[7];
+        IAID[2] = basemac[9];
+        IAID[3] = basemac[10];
+        IAID[4] = basemac[12];
+        IAID[5] = basemac[13];
+        IAID[6] = basemac[15];
+        IAID[7] = basemac[16];
+        IAID[8] = 0;
     }
 
-    /*
-       Sub-option code 6 - Software version (must match SW version field in SNMP MIB object sysDescr)
-    */
-    if (platform_hal_GetSoftwareVersion (buf, sizeof(buf)) == RETURN_OK)
+    IAID_HEX = strtol(IAID,NULL,16); //convert char to hex
+    //Identifier in format <OUI>"-"<ProductClass>"-"<CPE_LogisticsSerialNumber>
+
+    if(platform_hal_GetProductClass(ProductClass) != RETURN_OK)  //Product class is MERCV3X
     {
-        subopt = 6;
-        len = strlen (buf);
-        if (len > 0)
-        {
-            if ((len > 0xFF) || !verifyBufferSpace(length, opt_len, 2 + 2 + (2 * len))) {
-                return -1;
-            }
-            opt_len += sprintf(options + opt_len, "%02x%02x", subopt, len);
-            opt_len = writeTOHexFromAscii(options, length, opt_len, buf);
-        }
+        DBG_PRINT("Failed to get ProductClass \n");
+        return -1;
     }
 
-    /*
-       Sub-option code 7 - Boot ROM version (aka Bootloader version)
-    */
-    if (platform_hal_GetBootloaderVersion (buf, sizeof(buf)) == RETURN_OK)
+    if(platform_hal_GetSerialNumber(SerialNo) != RETURN_OK) //Serial Number
     {
-        subopt = 7;
-        len = strlen (buf);
-        if (len > 0)
-        {
-            if ((len > 0xFF) || !verifyBufferSpace(length, opt_len, 2 + 2 + (2 * len))) {
-                return -1;
-            }
-            opt_len += sprintf(options + opt_len, "%02x%02x", subopt, len);
-            opt_len = writeTOHexFromAscii(options, length, opt_len, buf);
-        }
+        DBG_PRINT("Failed to get SerialNumber \n");
+        return -1;
     }
 
-    /*
-       Sub-option code 8 - OUI
-    */
-    subopt = 8;
-    len = strlen (CONFIG_VENDOR_ID);
-    if (len > 0)
-    {
-        if ((len > 0xFF) || !verifyBufferSpace(length, opt_len, 2 + 2 + (2 * len))) {
-            return -1;
-        }
-        opt_len += sprintf(options + opt_len, "%02x%02x", subopt, len);
-        opt_len = writeTOHexFromAscii(options, length, opt_len, CONFIG_VENDOR_ID);
-    }
-
-    /*
-       Sub-option code 9 - Model Number
-    */
-    if (platform_hal_GetModelName(buf) == RETURN_OK)
-    {
-        subopt = 9;
-        len = strlen (buf);
-        if (len > 0)
-        {
-            if ((len > 0xFF) || !verifyBufferSpace(length, opt_len, 2 + 2 + (2 * len))) {
-              return -1;
-        }
-            opt_len += sprintf(options + opt_len, "%02x%02x", subopt, len);
-            opt_len = writeTOHexFromAscii(options, length, opt_len, buf);
-        }
-    }
-
-    /*
-       Sub-option code 10 - Vendor Name
-    */
-    subopt = 10;
-    len = strlen (CONFIG_VENDOR_NAME);
-    if (len > 0)
-    {
-        if ((len > 0xFF) || !verifyBufferSpace(length, opt_len, 2 + 2 + (2 * len))) {
-            return -1;
-        }
-        opt_len += sprintf(options + opt_len, "%02x%02x", subopt, len);
-        opt_len = writeTOHexFromAscii(options, length, opt_len, CONFIG_VENDOR_NAME);
-    }
-
-    /*
-       Sub-option code 15 - device eSAFE with cfg file encapsulation
-    */
-    subopt = 15;
-    len = strlen ("EROUTER");
-    if (len > 0)
-    {
-        if ((len > 0xFF) || !verifyBufferSpace(length, opt_len, 2 + 2 + (2 * len))) {
-            return -1;
-        }
-        opt_len += sprintf(options + opt_len, "%02x%02x", subopt, len);
-        opt_len = writeTOHexFromAscii(options, length, opt_len, "EROUTER");
-    }
+    snprintf(buf, sizeof(buf),"%s-%s-%s",CONFIG_VENDOR_ID,ProductClass,SerialNo);
+    opt_len += sprintf(options + opt_len, "%02x%0lx%02x%02x%02x%02x%x%02x",type,IAID_HEX,padding,DUID_TYPE,padding,padding,padding,atoi(DUID));
+    opt_len = writeTOHexFromAscii(options, length, opt_len, buf);
 
     return 0;
 }
@@ -426,7 +326,6 @@ static int get_dhcpv4_opt_list (dhcp_params * params, dhcp_opt_list ** req_opt_l
             char wanmg_enable[8];
 
             add_dhcpv4_opt_to_list(req_opt_list, DHCPV4_OPT_122, NULL);
-            add_dhcpv4_opt_to_list(req_opt_list, DHCPV4_OPT_125, NULL);
             add_dhcpv4_opt_to_list(req_opt_list, DHCPV4_OPT_43, NULL);
 
             syscfg_get(NULL, "management_wan_enabled", wanmg_enable, sizeof(wanmg_enable));
@@ -441,9 +340,16 @@ static int get_dhcpv4_opt_list (dhcp_params * params, dhcp_opt_list ** req_opt_l
                 {
                     DBG_PRINT("Failed to get OPTION 61 \n");
                 }
+
+                if (!prepare_dhcp125_optvalue(options, sizeof(options)))
+                {
+                    add_dhcpv4_opt_to_list(send_opt_list, DHCPV4_OPT_125, options);
+                }
+                else
+                {
+                    DBG_PRINT("Failed to get OPTION 125 \n");
+                }
                 add_dhcpv4_opt_to_list(send_opt_list, DHCPV4_OPT_60, "dslforum.org");
-                prepare_dhcp43_optvalue(options, sizeof(options), "true");
-                add_dhcpv4_opt_to_list(send_opt_list, DHCPV4_OPT_43, options);
             }
         }
     }
