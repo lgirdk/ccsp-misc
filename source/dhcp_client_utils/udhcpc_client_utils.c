3d7393efcc4d71469c077a6a5b4e452e4df7fc9f
mkamat <mkamat@libertyglobal.com>
2023-04-03 14:13:40 -0700

    dhcp_client_utils: fix DHCP Options missing or incorrect in DHCP Discover/Request Packets on eRouter WAN Interface ( OFW-4440 )

--- source/dhcp_client_utils/udhcpc_client_utils.c
+++ source/dhcp_client_utils/udhcpc_client_utils.c
@@ -126,6 +126,10 @@ static int udhcpc_get_send_options (char * buff, size_t buff_size, dhcp_opt_list
         {
             snprintf(args, sizeof(args), "-x 0x%02X:%s ", send_opt_list->dhcp_opt, send_opt_list->dhcp_opt_val);
         }
+        else if (send_opt_list->dhcp_opt == DHCPV4_OPT_43)
+        {
+            snprintf(args, sizeof(args), "-x %s ", send_opt_list->dhcp_opt_val);
+        }
         else
         {
 /*
