e4ae5fc1ea7796c0751dc001862f5bf3fd39c5d5
mkamat <mkamat@libertyglobal.com>
2023-04-03 14:13:40 -0700

    [Mv2+] DHCP Options are missing and incorrect in DHCP Discover/Request Packets on eRouter WAN Interface ( OFW-4440 )

--- source/dhcp_client_utils/udhcpc_client_utils.c
+++ source/dhcp_client_utils/udhcpc_client_utils.c
@@ -145,6 +145,10 @@ static int udhcpc_get_send_options (char * buff, dhcp_opt_list * send_opt_list)
         {
             snprintf(args, sizeof(args), "-x 0x%02X:%s ", send_opt_list->dhcp_opt, send_opt_list->dhcp_opt_val);
         }
+        else if (send_opt_list->dhcp_opt == DHCPV4_OPT_43)
+        {
+            snprintf(args, sizeof(args), "-x %s ", send_opt_list->dhcp_opt_val);
+        }
         else
         {
             char * buffer = ascii_to_hex (send_opt_list->dhcp_opt_val, strlen(send_opt_list->dhcp_opt_val));
