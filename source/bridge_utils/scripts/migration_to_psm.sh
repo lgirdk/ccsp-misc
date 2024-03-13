f210911972bcf3e0010f385d9faef63fb3b658c4
Fang, David <David_Fang@cable.comcast.com>
2024-03-13 23:27:33 +0000

    Revert "XB10–136: Sercomm XB10 modify model number"
    This reverts commit 86881a2bc55ccbc78adda62ce33a462d923a1136.
    
    Change-Id: I5df81c2bdc3dc1881a2b6a3912483207cc76be56

--- source/bridge_utils/scripts/migration_to_psm.sh
+++ source/bridge_utils/scripts/migration_to_psm.sh
@@ -40,11 +40,11 @@ migrationCompleteFlag=0
 cbr2_migrationCompleteFlag=0
 MIGRATION_FILE="/nvram/.migration_to_psm_complete"
 if [ "xcompleted" != "x`syscfg get psm_migration`" ];then
-	if [ "$MODEL_NUM" = "CGM4140COM" ] || [ "$MODEL_NUM" = "CGM4981COM" ] || [ "$MODEL_NUM" == "CGM601TCOM" ] ||  [ "$MODEL_NUM" == "SG417DB" ] || [ "$MODEL_NUM" = "CGM4331COM" ];then
+	if [ "$MODEL_NUM" = "CGM4140COM" ] || [ "$MODEL_NUM" = "CGM4981COM" ] || [ "$MODEL_NUM" == "CGM601TCOM" ] ||  [ "$MODEL_NUM" == "SG417DBCT" ] || [ "$MODEL_NUM" = "CGM4331COM" ];then
 		rm -rf "$MIGRATION_FILE"
 		psmcli set dmsb.l2net.1.Members.SW ""
 		psmcli set dmsb.l2net.9.Members.SW ""
-		if [ "$MODEL_NUM" = "CGM4331COM" ] || [ "$MODEL_NUM" = "CGM4981COM" ] || [ "$MODEL_NUM" == "CGM601TCOM" ] || [ "$MODEL_NUM" == "SG417DB" ];then
+		if [ "$MODEL_NUM" = "CGM4331COM" ] || [ "$MODEL_NUM" = "CGM4981COM" ] || [ "$MODEL_NUM" == "CGM601TCOM" ] || [ "$MODEL_NUM" == "SG417DBCT" ];then
 			psmcli set dmsb.l2net.6.Members.WiFi "wl0.3 wl1.3 wl0.5 wl1.5"
 		else
 			psmcli set dmsb.l2net.6.Members.WiFi "ath6 ath7 ath10 ath11"
@@ -60,14 +60,14 @@ if [ "xcompleted" != "x`syscfg get psm_migration`" ];then
 			fi
 		done
 		if [ "$PORT2ENABLE" = "1" ];then
-			if [ "$MODEL_NUM" = "CGM4331COM" ] || [ "$MODEL_NUM" = "CGM4981COM" ] || [ "$MODEL_NUM" == "CGM601TCOM" ] || [ "$MODEL_NUM" == "SG417DB" ];then
+			if [ "$MODEL_NUM" = "CGM4331COM" ] || [ "$MODEL_NUM" = "CGM4981COM" ] || [ "$MODEL_NUM" == "CGM601TCOM" ] || [ "$MODEL_NUM" == "SG417DBCT" ];then
 				psmcli set dmsb.l2net.1.Members.Eth "eth0 eth2 eth3"
 			else
 				psmcli set dmsb.l2net.1.Members.Eth "eth0"
 			fi
 			psmcli set dmsb.l2net.2.Members.Eth "eth1"
 		else
-			if [ "$MODEL_NUM" = "CGM4331COM" ] || [ "$MODEL_NUM" = "CGM4981COM" ] || [ "$MODEL_NUM" == "CGM601TCOM" ] || [ "$MODEL_NUM" == "SG417DB" ];then
+			if [ "$MODEL_NUM" = "CGM4331COM" ] || [ "$MODEL_NUM" = "CGM4981COM" ] || [ "$MODEL_NUM" == "CGM601TCOM" ] || [ "$MODEL_NUM" == "SG417DBCT" ];then
                         	psmcli set dmsb.l2net.1.Members.Eth "eth0 eth1 eth2 eth3"
                         else
 				psmcli set dmsb.l2net.1.Members.Eth "eth0 eth1"
@@ -143,7 +143,7 @@ fi
 #if device is FR in other builds which is not having bridgeUtil or OVS support, device will have wrong psm config. 
 #need to correct psm config 
 if [ "$migrationCompleteFlag" -eq 0 ];then
-	if [ "$MODEL_NUM" = "CGM4140COM" ] || [ "$MODEL_NUM" = "TG3482G" ] || [ "$MODEL_NUM" = "CGM4981COM" ] || [ "$MODEL_NUM" == "CGM601TCOM" ] || [ "$MODEL_NUM" == "SG417DB" ] || [ "$MODEL_NUM" = "CGM4331COM" ] || [ "$MODEL_NUM" = "TG4482A" ];then
+	if [ "$MODEL_NUM" = "CGM4140COM" ] || [ "$MODEL_NUM" = "TG3482G" ] || [ "$MODEL_NUM" = "CGM4981COM" ] || [ "$MODEL_NUM" == "CGM601TCOM" ] || [ "$MODEL_NUM" == "SG417DBCT" ] || [ "$MODEL_NUM" = "CGM4331COM" ] || [ "$MODEL_NUM" = "TG4482A" ];then
 		for i in 1 2
 		do
 			if [ "xl2sd0-t" = "x`psmcli get dmsb.l2net."$i".Members.Link`" ];then
